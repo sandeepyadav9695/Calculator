1ca877f39a69ec24914b56b419fb7e1bf2ae7c46
Sandeep Yadav <sy8673484@gmail.com>
2025-01-13 22:38:32 +0530

    Calculatar

---
+++ Calculator.java
@@ -0,0 +1,55 @@
+import java.util.Scanner;
+
+public class Calculator {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        while (true) {
+            System.out.println("Enter first number: ");
+            double num1 = scanner.nextDouble();
+
+            System.out.println("Enter second number: ");
+            double num2 = scanner.nextDouble();
+
+            System.out.println("Choose an operation (+, -, *, /) or 'q' to quit: ");
+            char operation = scanner.next().charAt(0);
+
+            if (operation == 'q') {
+                System.out.println("Calculator exited.");
+                break;
+            }
+
+            double result = 0;
+            boolean validOperation = true;
+
+            switch (operation) {
+                case '+':
+                    result = num1 + num2;
+                    break;
+                case '-':
+                    result = num1 - num2;
+                    break;
+                case '*':
+                    result = num1 * num2;
+                    break;
+                case '/':
+                    if (num2 != 0) {
+                        result = num1 / num2;
+                    } else {
+                        System.out.println("Cannot divide by zero.");
+                        validOperation = false;
+                    }
+                    break;
+                default:
+                    System.out.println("Invalid operation.");
+                    validOperation = false;
+            }
+
+            if (validOperation) {
+                System.out.println("The result is: " + result);
+            }
+        }
+
+        scanner.close();
+    }
+}
